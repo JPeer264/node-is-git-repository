7470b4606d08bd1100a4eea703f7abdef8e99ab9
JPeer264 <jan.oster94@gmail.com>
2017-05-04 23:03:02 +0200

    Feat: support relative paths

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-import { platform } from 'os';
 import execa from 'execa';
+import { platform } from 'os';
+import path from 'path';
+import pathIsAbsolute from 'path-is-absolute';
 
 const cwd = process.cwd();
 
 const isGit = (altPath = cwd) => {
+  const thisPath = pathIsAbsolute(altPath) ? altPath : path.join(cwd, altPath);
+
   try {
     if (platform() === 'win32') {
-      execa.shellSync(`pushd ${altPath} & git status`);
+      execa.shellSync(`pushd ${thisPath} & git status`);
     } else {
-      execa.shellSync(`(cd ${altPath} ; ([ -d .git ] && echo .git) || git rev-parse --git-dir 2> /dev/null)`);
+      execa.shellSync(`(cd ${thisPath} ; ([ -d .git ] && echo .git) || git rev-parse --git-dir 2> /dev/null)`);
     }
 
     return true;
