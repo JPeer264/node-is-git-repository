0dad339ec39f58747e1da41606e18ba21096b01e
JPeer264 <jan.oster94@gmail.com>
2017-05-06 19:06:42 +0200

    Fix: fix require() import of isGit (fixes #1)

--- test.js
+++ test.js
@@ -3,10 +3,14 @@ import test from 'ava';
 
 import isGit from './index';
 
+const isGitRequire = require('./');
+
 test('check if process.cwd() is a git repo', (t) => {
   t.true(isGit());
+  t.true(isGitRequire());
 });
 
 test('check if another dir is a git repo', (t) => {
   t.false(isGit(homedir()));
+  t.false(isGitRequire(homedir()));
 });
